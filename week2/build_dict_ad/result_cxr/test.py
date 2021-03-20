b8bba3397b29088998da26c5a1361d77c4dc3c6f
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-03-20 17:22:54 +0700

    update

--- week2/build_dict_ad/result_cxr/test.py
+++ week2/build_dict_ad/result_cxr/test.py
@@ -1,11 +1,15 @@
 import json
+import re
 with open("list_long_form_cxrv2.txt", "r") as f:
     dict_file = {}
     for i, line in enumerate(f.readlines()):
-        if len(line.split(" ")) == 1 : dict_file[f"None_{i}"] = line
+        if len(line.split(" ")) == 1 : 
+            line = re.sub("\\n", "", line)
+            dict_file[f"None_{i}"] = line
         else:
             token = line.split(" ")
             acronym = "".join([j[0] for j in token])
+            line = re.sub("\\n", "", line)
             dict_file[acronym] = line
     # print(dict_file)
 with open("dict_acronym_cxrv2.json", "w", encoding="utf8") as f:
